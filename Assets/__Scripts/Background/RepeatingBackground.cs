95e4081582d1af9d6152e3a8858e41f146d7e4d7
Rachel <g00337231@gmit.ie>
2019-10-16 18:05:48 +0100

    fixing issues with background skipping

---
+++ Assets/__Scripts/Background/RepeatingBackground.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingBackground : MonoBehaviour
+{
+    private BoxCollider2D bgCollider;
+    private float bgSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bgCollider = GetComponent<BoxCollider2D>();
+        bgSize = bgCollider.size.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < bgSize)
+        {
+            RepeatBackground();
+        }
+    }
+
+    private void RepeatBackground()
+    {
+        Vector2 offset = new Vector2(0, bgSize * 2f);
+        transform.position = (Vector2)transform.position + offset;
+    }
+}
---
+++ Assets/__Scripts/Player/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 10.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        // get a change in direction
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+
+        //calculate a new x position
+        var newXPos = transform.position.x + deltaX;
+
+        transform.position = new Vector2(newXPos, transform.position.y);
+    }
+}

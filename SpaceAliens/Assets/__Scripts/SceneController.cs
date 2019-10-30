0c5aae19197aca5771eaee7b67ef0447864bb43f
Rachel <g00337231@gmit.ie>
2019-10-30 20:00:46 +0000

    working on main screen

---
+++ SpaceAliens/Assets/__Scripts/SceneController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    // methods for buttons
+    public void OnClickPlay()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
